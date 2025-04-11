e9b57f60a4015341a458ef6a3f950773d5004138
eman <eman@gmail.com>
2025-04-11 19:18:06 +0200

    add

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
  import 'bootstrap/dist/js/bootstrap.min.js'
 import { Provider } from 'react-redux';
@@ -11,7 +11,7 @@ import { mystore } from './redux/store';
  const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
           <Provider store={mystore}>
           <App/>
 
@@ -20,7 +20,7 @@ root.render(
             </Provider>    
     
     
-    </BrowserRouter>
+    </HashRouter>
    </React.StrictMode>
 );
 
